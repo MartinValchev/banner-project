e90ef1d28265dd8b76fb45967d32b2bb3c663fb5
mvalchev <martinvalchev1@gmail.com>
2025-08-24 21:36:29 +0300

    initial commit

---
+++ banner-project/src/main/java/com/website/campaings/banner_project/BannerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.website.campaings.banner_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerProjectApplication.class, args);
+	}
+
+}
